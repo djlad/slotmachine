a93089da97616bf33c3b5bb16090029d535225a9
firefly <firefly@fireflys-Air.home>
2018-12-08 17:09:30 -0500

    added dummy data

--- slotmachineserver/slotmachineservices/urls.py
+++ slotmachineserver/slotmachineservices/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('runslotmachine', views.run_slot_machine, name='index')
+    path('runslotmachine', views.run_slot_machine, name='runmachine'),
+    path('login', views.login, name="login")
     ]
--- slotmachineserver/slotmachineservices/views.py
+++ slotmachineserver/slotmachineservices/views.py
@@ -1,9 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import json
 
 # Create your views here.
 def index(request):
     return HttpResponse("index pages is good ")
 
 def run_slot_machine(request):
-    return HttpResponse("running slot machine")
+    response = {
+        "result":[1,2,3]
+    }
+    data = json.dumps(response)
+    return HttpResponse(data, content_type='application/json') 
+
+def login(request):
+    response = {
+        "token":"thisisatoken"
+    }
+    data = json.dumps(response)
+    return HttpResponse(data, content_type='application/json') 
